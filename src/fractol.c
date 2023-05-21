33cc844ce5e774c7752b6ab58c8eb8806ca7e224
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2023-05-21 09:41:07 +0300

    color and organization

--- src/fractol.c
+++ src/fractol.c
@@ -1,12 +1,18 @@
-#include "../lib/mlx_lib/mlx.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mugurel <muhammedtalhaugurel@gmai...>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 02:38:10 by mugurel           #+#    #+#             */
+/*   Updated: 2022/12/23 05:35:40 by mugurel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+#include "../lib/mlx_lib/mlx.h"
+#include "fractol.h"
+
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -16,68 +22,67 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-int	create_trgb(int t, int r, int g, int b)
+int	create_trgb(int t, int r, int g, int b, int iter)
 {
+	t = 180;
+	r = iter;
+	g = iter;
+	b = iter;
+	r *= 10;
+	g *= 15;
+	b *= 20;
+	r %= 256;
+	g %= 256;
+	b %= 256;
+
 	return (t << 24 | r << 16 | g << 8 | b);
-}
-
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
-
-int	close(int keycode, t_vars *vars)
-{
-	return (0);
 }
 
 int	main(void)
 {
-	t_data	*img;
+	t_data	img;
 	t_vars	vars;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 400, 400, "MTU");
-	img->img = mlx_new_image(vars.mlx, 400, 400);
-	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length,
-								&img->endian);
-	fractol(img);
-	mlx_put_image_to_window(vars.mlx, vars.win, img->img, 0, 0);
-	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+	vars.win = mlx_new_window(vars.mlx, 1080, 600, "MTU");
+	img.img = mlx_new_image(vars.mlx, 1080, 600);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	fractol(&img);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 	mlx_loop(vars.mlx);
 }
 
 void	fractol(t_data *img)
 {
-	int	x;
-	int	y;
-
-	x = 0;
-	y = 0;
-	while (x < 400)
+	int	row;
+	int	col;
+	int width;
+	int height;
+	
+	row = 0;
+	col = 0;
+	width = 1080;
+	height = 600;
+	while (row < 600)
 	{
-		while (y < 200)
+		while (col < 1080)
 		{
-			y++;
+	    double c_re = (col - width/2.0)*4.0/width;
+	    double c_im = (row - height/2.0)*4.0/width;
+	    double x = 0, y = 0;
+	    int iteration = 0;
+	    while (x*x+y*y <= 4 && iteration < 255) {
+	        double x_new = x*x - y*y + c_re;
+	        y = 2*x*y + c_im;
+	        x = x_new;
+	        iteration++;
+	    }
+	    if (iteration < 255)
+				my_mlx_pixel_put(img, col, row, create_trgb(255, 255, 255, 255, iteration));
+			col++;
 		}
-		y = 0;
-		x++;
+		col = 0;
+		row++;
 	}
-	int width = 400;
-	int height = 400;
-	for (int row = 0; row < height; row++) {
-	    for (int col = 0; col < width; col++) {
-	        double c_re = (col - width/2.0)*4.0/width;
-	        double c_im = (row - height/2.0)*4.0/width;
-	        double x = 0, y = 0;
-	        int iteration = 0;
-	        while (x*x+y*y <= 4 && iteration < 400) {
-	            double x_new = x*x - y*y + c_re;
-	            y = 2*x*y + c_im;
-	            x = x_new;
-	            iteration++;
-	        }
-	        if (iteration < 100) my_mlx_pixel_put(img, col, row, create_trgb(255,255,255,255));
-    }
-}
 }
--- src/mlx_utils.c
+++ src/mlx_utils.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.c                                          :+:      :+:    :+:   */
+/*   mlx_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mugurel <muhammedtalhaugurel@gmai...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -9,23 +9,3 @@
 /*   Updated: 2022/12/23 05:35:40 by mugurel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef FRACTOL_H
-# define FRACTOL_H
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
-
-void	fractol(t_data *img);
-
-#endif
